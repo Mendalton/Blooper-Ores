23e4c95396225dee7ce4c75102986a3205419504
Avin Abeysinghe <76015314+Mendalton@users.noreply.github.com>
2021-06-13 14:38:23 -0700

    added mod stuff

---
+++ src/main/java/net/blooperores/blooperOres.java
@@ -0,0 +1,18 @@
+package net.blooperores;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class blooperOres implements ModInitializer {
+
+    public static final String MODID = "blooperores" ;
+
+    public static final Item BLOOPER_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+    @Override
+    public void onInitialize(){
+        Registry.register(Registry.ITEM, new Identifier(MODID, "blooper_item"), BLOOPER_ITEM);
+    }
+}
